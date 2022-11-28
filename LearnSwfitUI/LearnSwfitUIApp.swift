b13b48405ac4319c8207ab1e80c510c4ea461515
WUYUEZONG <w.yzong@outlook.com>
2022-11-28 10:34:58 +0800

    Initial Commit

---
+++ LearnSwfitUI/LearnSwfitUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwfitUIApp.swift
+//  LearnSwfitUI
+//
+//  Created by mntechMac on 2022/11/28.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwfitUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
